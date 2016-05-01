fa7dfa48a04143e3dfd42c80454d9a046a637343
Simon Diemert <simon.diemert@gmail.com>
2016-05-01 16:29:58 -0700

    basic framework setup, sample Watson AI.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
+"use strict";
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.send("foobar");
+var AIUtils = require('../lib/AIUtils.js');
+var Watson = require("../lib/WatsonAI.js");
+
+/* The structure of request.body is:
+ * { board : [[number, ...], ...], size : number, last : { x : number, y : number } } }
+ *
+ * Response payload will be:
+ * { x : number, y : number, color: number }
+ */
+
+router.post('/watson', function (req, res, next) {
+
+    var B = AIUtils.boardFromRequest(req.body);
+    var last = AIUtils.lastMoveFromRequest(req.body);
+
+    var W = new Watson();
+    
+    res.json(W.move(B, last));
+
 });
 
 module.exports = router;
